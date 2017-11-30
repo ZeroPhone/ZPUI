6075f3da513e618a994b03a2c1a9326a3621f048
ahub <monsieur_h@riseup.net>
2017-11-30 08:24:54 +0100

    PEP 8 formatting removes unused imports

--- apps/example_apps/skeleton/main.py
+++ apps/example_apps/skeleton/main.py
@@ -1,31 +1,34 @@
-menu_name = "Skeleton app" #App name as seen in main menu while using the system
+menu_name = "Skeleton app"  # App name as seen in main menu while using the system
 
 from subprocess import call
-from time import sleep
 
 from ui import Menu, Printer
+
 
 def call_internal():
     Printer(["Calling internal", "command"], i, o, 1)
     print("Success")
 
+
 def call_external():
     Printer(["Calling external", "command"], i, o, 1)
     call(['echo', 'Success'])
 
-#Callback global for ZPUI. It gets called when application is activated in the main menu
+
+# Callback global for ZPUI. It gets called when application is activated in the main menu
 callback = None
 
-i = None #Input device
-o = None #Output device
+i = None  # Input device
+o = None  # Output device
+
 
 def init_app(input, output):
     global callback, i, o
-    i = input; o = output #Getting references to output and input device objects and saving them as globals
+    i = input;
+    o = output  # Getting references to output and input device objects and saving them as globals
     main_menu_contents = [
-    ["Internal command", call_internal],
-    ["External command", call_external],
-    ["Exit", 'exit']]
+        ["Internal command", call_internal],
+        ["External command", call_external],
+        ["Exit", 'exit']]
     main_menu = Menu(main_menu_contents, i, o, "Skeleton app menu")
     callback = main_menu.activate
-
--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python2
-import sys
-import os
-from subprocess import call
-from time import sleep
 import argparse
-#Welcome to ZPUI innards
-#Here, things are about i and o, which are input and output
-#And we output things for debugging, so o goes first.
+import os
+
+# Welcome to ZPUI innards
+# Here, things are about i and o, which are input and output
+# And we output things for debugging, so o goes first.
 from output import output
 
 main_config_path = "/boot/zpui_config.json"
@@ -14,11 +12,13 @@ backup_config_path = "./config.json"
 config_error_file = "/boot/zpui_config_error.repr"
 emulator_flag_filename = "emulator"
 
-#Debugging helper snippet
+# Debugging helper snippet
 import threading
 import traceback
 import signal
 import sys
+
+
 def dumpthreads(*args):
     print("")
     print("SIGUSR received, dumping threads")
@@ -27,12 +27,14 @@ def dumpthreads(*args):
         print(th)
         traceback.print_stack(sys._current_frames()[th.ident])
         print("")
+
+
 signal.signal(signal.SIGUSR1, dumpthreads)
 
-#Getting ZPUI config, it will be passed to input and output initializers
-#If config at main_config_path exists, use that
-#If not, use the backup path
-#Also, log the errors to a file so that it can be debugged later
+# Getting ZPUI config, it will be passed to input and output initializers
+# If config at main_config_path exists, use that
+# If not, use the backup path
+# Also, log the errors to a file so that it can be debugged later
 
 from helpers import read_config
 
@@ -40,7 +42,7 @@ is_emulator = emulator_flag_filename in os.listdir(".")
 
 if not is_emulator:
     try:
-        error_file = open(config_error_file, "w+")
+        error_file = open(config_error_file, "w+")  # todo : why wrtie permission?
         config = read_config(main_config_path)
     except Exception as e:
         print(repr(e))
@@ -62,23 +64,23 @@ if not is_emulator:
 else:
     config = read_config(backup_config_path)
 
-
 output.init(config["output"])
 o = output.screen
 from ui import Printer, Menu
 
-
-try: #If there's an internal error, we show it on display and exit
+try:  # If there's an internal error, we show it on display and exit
     from apps.manager import AppManager
-    #Now we init the input subsystem
+    # Now we init the input subsystem
     from input import input
+
     input.init(config["input"])
     i = input.listener
     if hasattr(o, "set_backlight_callback"):
         o.set_backlight_callback(i)
 except:
-    Printer(["Oops. :(", "y u make mistake"], None, o, 0) #Yeah, that's about all the debug data. 
+    Printer(["Oops. :(", "y u make mistake"], None, o, 0)  # Yeah, that's about all the debug data.
     raise
+
 
 def splash_screen():
     try:
@@ -86,6 +88,7 @@ def splash_screen():
         splash(i, o)
     except ImportError:
         pass
+
 
 def exception_wrapper(callback):
     """This is a wrapper for all applications and menus. It catches exceptions and stops the system the right way when something bad happens, be that a Ctrl+c or an exception in one of the applications."""
@@ -105,11 +108,13 @@ def exception_wrapper(callback):
         i.atexit()
         sys.exit(0)
 
+
 def launch(name=None):
     """Function that launches ZPUI, either in full mode or single-app mode (if ``name`` kwarg is passed)."""
     app_man = AppManager("apps", Menu, Printer, i, o)
     if name != None:
-        name = name.rstrip('/') #If using autocompletion from main folder, it might append a / at the name end, which isn't acceptable for load_app
+        name = name.rstrip(
+            '/')  # If using autocompletion from main folder, it might append a / at the name end, which isn't acceptable for load_app
         try:
             app = app_man.load_app(name)
         except:
@@ -121,8 +126,10 @@ def launch(name=None):
         app_menu = app_man.load_all_apps()
         exception_wrapper(app_menu.activate)
 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="ZPUI runner")
-    parser.add_argument('-a', '--app', action="store", help="Launch ZPUI with a single app loaded (useful for testing)", default=None)
+    parser.add_argument('-a', '--app', action="store", help="Launch ZPUI with a single app loaded (useful for testing)",
+                        default=None)
     args = parser.parse_args()
     launch(name=args.app)
