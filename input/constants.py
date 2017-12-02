1c60afa3e6063fc4bf55a516498f52ef3fc8b26c
monsieur_h <monsieur_h@riseup.net>
2017-12-02 21:38:51 +0100

    Adds a base constant class to start discussion

---
+++ input/constants.py
@@ -0,0 +1,37 @@
+from enum import Enum
+
+
+class KeyCodes(Enum):
+    KEY_LEFT = "KEY_LEFT"
+    KEY_UP = "KEY_UP"
+    KEY_DOWN = "KEY_DOWN"
+    KEY_RIGHT = "KEY_RIGHT"
+    KEY_ENTER = "KEY_ENTER"
+    KEY_1 = "KEY_1"
+    KEY_2 = "KEY_2"
+    KEY_3 = "KEY_3"
+    KEY_4 = "KEY_4"
+    KEY_5 = "KEY_5"
+    KEY_6 = "KEY_6"
+    KEY_7 = "KEY_7"
+    KEY_8 = "KEY_8"
+    KEY_9 = "KEY_9"
+    KEY_STAR = "KEY_*"
+    KEY_0 = "KEY_0"
+    KEY_SHARP = "KEY_#"
+    KEY_F1 = "KEY_F1"
+    KEY_F2 = "KEY_F2"
+    KEY_ANSWER = "KEY_ANSWER"
+    KEY_HANGUP = "KEY_HANGUP"
+    KEY_PAGEUP = "KEY_PAGEUP"
+    KEY_PAGEDOWN = "KEY_PAGEDOWN"
+    KEY_F5 = "KEY_F5"
+    KEY_F6 = "KEY_F6"
+    KEY_VOLUMEUP = "KEY_VOLUMEUP"
+    KEY_VOLUMEDOWN = "KEY_VOLUMEDOWN"
+    KEY_PROG1 = "KEY_PROG1"
+    KEY_PROG2 = "KEY_PROG2"
+    KEY_CAMERA = "KEY_CAMERA"
+
+    # todo: discuss aliases
+    # example : KEY_VALIDATE = KEY_CALL
