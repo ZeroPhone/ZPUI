2d3d71201ae5e9f27ab93f3ab4c251ac7e20a4cb
CRImier <crimier@yandex.ru>
2025-05-25 21:01:53 +0200

    apps/hardware_apps/usb: app now can load on non-ZP Raspberry Pi and other SoC devices (for USB root port control)

--- apps/hardware_apps/usb/main.py
+++ apps/hardware_apps/usb/main.py
@@ -2,7 +2,14 @@ from zpui_lib.helpers import setup_logger
 
 menu_name = "USB control"
 
-from zerophone_hw import USB_DCDC
+zerophone_hw = False
+try:
+    from zerophone_hw import USB_DCDC
+except:
+    pass
+else:
+    zerophone_hw = True
+
 from ui import Menu, Printer
 from actions import BackgroundAction
 from time import sleep
@@ -13,8 +20,33 @@ i = None
 o = None
 context = None
 
-dcdc = USB_DCDC()
-dcdc_state = False
+if zerophone_hw:
+    dcdc = USB_DCDC()
+    dcdc_state = False
+
+usb_file = None
+usb_file_base_dir = "/sys/devices/platform/soc/"
+usb_control_file = "buspower"
+usb_full_path = None
+
+def can_load():
+    # currently the app won't work with anything other than zerophone.
+    # that's wrong ngl
+    global usb_file, usb_full_path, failed_to_load
+    #Find the usb device control directory
+    if not os.path.exists(usb_file_base_dir):
+        return False, "cannot find USB base dir: {} (not running on an SBC?)".format(usb_file_base_dir)
+    device_files = os.listdir(usb_file_base_dir)
+    usb_files = [file for file in device_files if file.endswith(".usb")]
+    if not usb_files:
+        return False, "cannot find USB control file in USB base dir: {}".format(usb_file_base_dir)
+    usb_file = usb_files[0] #I'm guessing having more than one file would mean
+    #having more than one USB controller, so this is not Raspberry Pi stuff anymore
+    #and I can only test this on a Pi right now.
+    usb_full_path = os.path.join(usb_file_base_dir, usb_file, usb_control_file)
+    if not os.path.exists(usb_full_path):
+        return False, "cannot find file {} in {}".format(usb_control_file, usb_full_path)
+    return True
 
 def get_menu_name():
     return "USB on" if dcdc_state else "USB off"
@@ -45,11 +77,6 @@ def dcdc_off():
 def dcdc_toggle():
     dcdc_off() if dcdc_state else dcdc_on()
 
-usb_file = None
-usb_file_base_dir = "/sys/devices/platform/soc/"
-usb_control_file = "buspower"
-usb_full_path = None
-
 def usb_off_on():
     with open(usb_full_path, "w") as f:
         f.write("0")
@@ -64,32 +91,17 @@ def usb_off():
     with open(usb_full_path, "w") as f:
         f.write("0")
 
-main_menu_contents = [ 
-["Restart 5V DC-DC", dcdc_off_on],
-["Turn 5V DC-DC on", dcdc_on],
-["Turn 5V DC-DC off", dcdc_off],
-["Restart USB bus", usb_off_on],
-["Turn USB bus on", usb_on],
-["Turn USB bus off", usb_off]
-]
 
-def init_app(input, output):
-    global i, o, usb_file, usb_full_path
-    i = input; o = output
-    #Find the usb device control directory
-    device_files = os.listdir(usb_file_base_dir)
-    usb_files = [file for file in device_files if file.endswith(".usb")]
-    if not usb_files:
-        logger.error("Can't find suitable USB file at {}! What's wrong with this hardware?".format(usb_file_base_dir))
-        raise IOError
-    usb_file = usb_files[0] #I'm guessing having more than one file would mean 
-    #having more than one USB controller, so this is not Raspberry Pi stuff anymore
-    #and I can only test this on a Pi right now.
-    usb_full_path = os.path.join(usb_file_base_dir, usb_file, usb_control_file)
-    if not os.path.exists(usb_full_path):
-        logger.error("Can't find {} file at {}! What's wrong with this hardware?".format(usb_control_file, usb_full_path))
-        raise IOError
-        
 def callback():
+    main_menu_contents = [
+        ["Restart USB bus", usb_off_on],
+        ["Turn USB bus on", usb_on],
+        ["Turn USB bus off", usb_off]
+    ]
+    zp_items = [
+        ["Restart 5V DC-DC", dcdc_off_on],
+        ["Turn 5V DC-DC on", dcdc_on],
+        ["Turn 5V DC-DC off", dcdc_off],
+    ]
+    # TODO: actually detect zerophone hardware and add ZP items
     Menu(main_menu_contents, i, o, "USB app menu").activate()
-
