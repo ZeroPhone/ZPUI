e83f0a66922497085688fea5dc00d8e4a72d31bf
CRImier <crimier@yandex.ru>
2025-05-29 03:28:26 +0200

    apps/network_apps/wpa_cli: fixing syntax error (I am staying up way too late)

--- apps/network_apps/wpa_cli/main.py
+++ apps/network_apps/wpa_cli/main.py
@@ -9,7 +9,7 @@ from zpui_lib.helpers import setup_logger, ExitHelper
 from actions import FirstBootAction as FBA
 from zpui_lib.libs.linux.wpa_monitor import WpaMonitor
 from ui import Menu, PrettyPrinter as Printer, MenuExitException, UniversalInput, \
-               Refresher, DialogBox, ellipsize, Entry, LoadingBar, SpinnerOverlay. Canvas
+               Refresher, DialogBox, ellipsize, Entry, LoadingBar, SpinnerOverlay, Canvas
 
 import net_ui
 import read_conf_data
