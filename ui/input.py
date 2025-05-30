3b17fb138411c4fa3f95c53531995ecc8d796df9
CRImier <crimier@yandex.ru>
2025-05-30 22:46:36 +0200

    ui/input: advanced debugging for UniversalInput choice heuristics

--- ui/input.py
+++ ui/input.py
@@ -1,5 +1,8 @@
 from ui.numpad_input import NumpadCharInput, NumpadNumberInput, NumpadHexInput, NumpadKeyboardInput, NumpadPasswordInput
 from ui.char_input import CharArrowKeysInput
+from zpui_lib.helpers import setup_logger
+logger = setup_logger(__name__, "warning")
+
 
 def UniversalInput(i, o, *args, **kwargs):
     """
@@ -10,9 +13,11 @@ def UniversalInput(i, o, *args, **kwargs):
     TODO: document arguments (most of them are passed through, like "name" or "message")
     """
     charmap = kwargs.pop("charmap", "full")
+    name = kwargs.get("name", "!!!noname!!!")
     # Determining which input is necessary, according to the charmap requested
     numpadinputs = {"full":NumpadCharInput, "number":NumpadNumberInput, "hex":NumpadHexInput, "password":NumpadPasswordInput}
     numpadinput_cls = numpadinputs[charmap]
+    logger.debug("{}: using charmap {}".format(name, charmap))
     # What goes here for NumpadKeyboardInput
     arrowkeyinput_maps = {"full":['][S', '][c', '][C', '][s', '][n'], "number":['][n'], "hex":['][h']}
     arrowkeyinput_maps["password"] = arrowkeyinput_maps["full"]
@@ -21,6 +26,7 @@ def UniversalInput(i, o, *args, **kwargs):
     if None in i.available_keys.values():
         # HID driver (or other driver with "any key is possible" is likely used
         # Let's use the most fully-functional input available at the moment
+        logger.info("{}: None in available_keys found; returning the NumpadKeyboardInput".format(name, charmap))
         return numpadinput_cls(i, o, *args, **kwargs)
     all_available_keys = sum(i.available_keys.values(), [])
 
@@ -28,16 +34,21 @@ def UniversalInput(i, o, *args, **kwargs):
     ascii_keys_available = all([ascii_key in all_available_keys for ascii_key in ascii_keys])
     action_keys = ["KEY_F1", "KEY_F2"]
     action_keys_available = all([action_key in all_available_keys for action_key in action_keys])
+    logger.debug("{}: ascii: {}, action: {}".format(name, ascii_keys_available, action_keys_available))
     if ascii_keys_available and action_keys_available:
         # All required ASCII and action keys are supported
+        logger.info("{}: All ASCII and action keys are supported; returning the NumpadKeyboardInput".format(name))
         return NumpadKeyboardInput(i, o, *args, **kwargs)
 
     number_keys = ["KEY_{}".format(x) for x in range(10)]
     number_keys.append("KEY_*")
     number_keys.append("KEY_#")
     number_keys_available = all([number_key in all_available_keys for number_key in number_keys ])
+    logger.debug("{}: number: {}".format(name, number_keys_available))
     if number_keys_available and action_keys_available:
         # All number and action keys are supported
+        logger.info("{}: All number and action keys are supported; returning the uhhh {}?".format(name, str(numpadinput_cls)))
         return numpadinput_cls(i, o, *args, **kwargs)
     # Fallback - only needs five primary keys
+    logger.info("{}: fallback; returning CharArrowKeysInput".format(name))
     return CharArrowKeysInput(i, o, allowed_chars=arrowkeyinput_map, *args, **kwargs)
