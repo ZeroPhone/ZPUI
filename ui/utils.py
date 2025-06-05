91a2339bdb801e768cf3226fb4856888e7980361
CRImier <crimier@yandex.ru>
2025-06-05 02:21:18 +0200

    ui/utils: better background color forwarding in fit_image_to_*

--- ui/utils.py
+++ ui/utils.py
@@ -222,7 +222,7 @@ class Ticker(object):
 
 Rect = namedtuple('Rect', ['left', 'top', 'right', 'bottom'])
 
-def fit_image_to_screen(image, o, resampling=Image.BOX):
+def fit_image_to_screen(image, o, resampling=Image.BOX, fill_color="black"):
     """Fits a given image to fit on any sized screen whilst maintaining the aspect ratio.
     Any remaining space is filled with borders. The resized image is returned as ``image``.
 
@@ -232,7 +232,7 @@ def fit_image_to_screen(image, o, resampling=Image.BOX):
         * ``o``: output device object. Used to find the width and height of the screen.
         * ``resampling=PIL.Image.BOX``: PIL resampling algorithm to be used during resizing.
         See https://pillow.readthedocs.io/en/stable/handbook/concepts.html#concept-filters ."""
-    return fit_image_to_dims(image, o.width, o.height, resampling=resampling)
+    return fit_image_to_dims(image, o.width, o.height, resampling=resampling, fill_color=fill_color)
 
 def fit_image_to_dims(image, width, height, resampling=Image.BOX, fill_color = "black"):
     """Fits a given image to fit on any sized screen whilst maintaining the aspect ratio.
