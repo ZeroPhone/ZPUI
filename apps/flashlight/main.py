a7afa7563f1d594c43ce352667742871359ccae5
CRImier <crimier@yandex.ru>
2025-10-05 15:54:24 +0200

    apps/flashlight: adding a Settings-discoverable setting toggle, allowing to disable individual elements of the flashlight app

--- apps/flashlight/main.py
+++ apps/flashlight/main.py
@@ -3,8 +3,8 @@ menu_name = "Flashlight"
 import os
 from time import sleep
 
-from zpui_lib.ui import Canvas
-from zpui_lib.helpers import ExitHelper, get_platform
+from zpui_lib.ui import Canvas, Menu, Checkbox, PrettyPrinter as Printer
+from zpui_lib.helpers import ExitHelper, get_platform, read_or_create_config, local_path_gen, save_config_gen
 from zpui_lib.actions import ContextSwitchAction as Action
 
 zerophone_hw = False
@@ -20,11 +20,18 @@ state = False
 context = None
 i = None; o = None
 
+local_path = local_path_gen(__name__)
+
+default_config = "disabled_lights: []"
+config = read_or_create_config(local_path("config.yaml"), default_config, menu_name+" app")
+save_config = save_config_gen(local_path("config.yaml"))
+
 def can_load():
     # maybe in the future we can check if the screen has backlit output, and if any LEDs are available
     return True
 
 # we don't even need this function right now, really
+
 """
 def init_app(input, output):
     global i, o, led
@@ -41,6 +48,40 @@ def set_context(c):
     if can_load() == True:
         context = c
         context.register_action(Action("flashlight_toggle", c.request_switch, menu_name=get_state_message, description="Switching to the flashlight app"))
+        context.set_provider("settings_flashlight", flashlight_settings)
+
+def get_platform_lights():
+    platform = get_platform()
+    lights = ["screen"]
+    if "beepy" in platform:
+        #lights.append("beepy_backlight") # currently not implemented but will be
+        lights.append("beepy_led")
+    return lights
+
+def flashlight_toggles():
+    lights = get_platform_lights()
+    contents = {light:True for light in lights}
+    for light in config["disabled_lights"]:
+        contents[light] = False
+    cb_contents = []
+    for name, state in contents.items():
+        hr_name = name.replace("_", " ").capitalize()
+        cb_contents.append([hr_name, name, state])
+    cb = Checkbox(cb_contents, i, o, name="Flashlight app toggles setting")
+    result = cb.activate()
+    if result:
+        for name, state in result.items():
+            if not state:
+                if name not in  config["disabled_lights"]:
+                    config["disabled_lights"].append(name)
+            elif state and name in config["disabled_lights"]: # enabled but included in disabled items, removing
+                config["disabled_lights"].remove(name)
+        save_config(config)
+
+def flashlight_settings():
+    return flashlight_toggles()
+
+flashlight_settings.name = "Flashlight settings"
 
 def get_state_message():
     if state:
@@ -66,16 +107,22 @@ def set_led(new_state):
     """
     # do we have the Beepy LED available?
     if "beepy" in get_platform():
-        if new_state: # LED on
-            write_beepy_led("led_red", 100)
-            write_beepy_led("led_green", 100)
-            write_beepy_led("led_blue", 100)
-            write_beepy_led("led", 1)
-        else:
-            write_beepy_led("led", 0)
+        if "beepy_led" not in config["disabled_lights"]:
+            if new_state: # LED on
+                write_beepy_led("led_red", 100)
+                write_beepy_led("led_green", 100)
+                write_beepy_led("led_blue", 100)
+                write_beepy_led("led", 1)
+            else:
+                write_beepy_led("led", 0)
 
 def callback():
     global state
+    # check - are all possible lights disabled?
+    lights = get_platform_lights()
+    if all([light in config["disabled_lights"] for light in lights]):
+        Printer("All available lights disabled!", i, o, 3)
+        return
     # do we have a color screen? if so, block
     # first thing to do: toggle the LED
     state = not state
@@ -83,19 +130,21 @@ def callback():
     # now, fill the canvas with white
     # damn, wish I could filter this out on the Memory LCD
     # if this can be filtered out, this would be the spot that callback() would stop executing in
-    c = Canvas(o, interactive=True)
-    if state: # new state
-        c.clear(fill="white") # hardcoded as flashlight colour
-    else:
-        c.clear(fill="black")
-    eh = ExitHelper(i, keys=["KEY_ENTER", "KEY_LEFT"]).start()
-    while eh.do_run():
-        sleep(0.1)
-    last_key = eh.last_key
-    if last_key == "KEY_ENTER":
-        # flip the LED before exiting
-        set_led(not state)
-        state = not state
-        c.clear(fill="white" if state else "black")
+    if "screen" not in config["disabled_lights"]:
+        c = Canvas(o, interactive=True)
+        if state: # new state
+            c.clear(fill="white") # hardcoded as flashlight colour
+        else:
+            c.clear(fill="black")
+    if state and "screen" not in config["disabled_lights"]:
+        eh = ExitHelper(i, keys=["KEY_ENTER", "KEY_LEFT"]).start()
+        while eh.do_run():
+            sleep(0.1)
+        last_key = eh.last_key
+        if last_key == "KEY_ENTER":
+            # flip the LED before exiting
+            set_led(not state)
+            state = not state
+            c.clear(fill="white" if state else "black")
     # todo: if no external LED is supported, disable flashlight on exit
-    # in the future, we could add KEY_RIGHT and use that for flashlight settings owo
+    # in the future, we could add KEY_RIGHT and use that for flashlight settings? would be fun maybe
