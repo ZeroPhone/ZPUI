ffc7fcd28580292bf3815efd4f47ea701c477e13
CRImier <crimier@yandex.ru>
2025-10-10 20:49:24 +0200

    apps/settings/logging_ui: cleaning up the log level dialog, making it explicit and visible which logging levels have been changed from the default

--- apps/settings/logging_ui.py
+++ apps/settings/logging_ui.py
@@ -20,6 +20,9 @@ from zpui_lib.ui import Menu, Listbox
 def get_logger_names():
     return log_system.get_logger_names()
 
+def get_initial_logger_state():
+    return log_system.get_initial_logger_state()
+
 def prettify_logger_names(names):
     name_mapping = OrderedDict()
     for name in names:
@@ -65,14 +68,35 @@ def change_loglevel(logger_name, current_level):
         log_system.LoggingConfig().set_level(logger_name, new_level)
         log_system.LoggingConfig().reload_config()
 
+shorthands = {"NOTSET":"n"}
+
 def get_menu_contents():
     logger_names = get_logger_names()
+    initial_logger_states = get_initial_logger_state()
     pretty_logger_names = prettify_logger_names(logger_names)
     sorted_names = sorted(pretty_logger_names.items(), key=lambda x: x[1])
     for i, (name, pname) in enumerate(sorted_names):
         l = log_system.get_log_level_for_logger(name)
-        sorted_names[i] = (name, pname, l)
-    return [["{}:{}".format(l[:1], pname), lambda x=name, y=l:change_loglevel(x, y)] for name, pname, l in sorted_names]
+        orig_l = initial_logger_states.get(name, l).upper()
+        sorted_names[i] = (name, pname, l, orig_l)
+    mc = []
+    # first pass, append the levels which have changed from the default
+    for name, pname, l, orig_l in sorted_names:
+        shorthand = shorthands.get(l, l[:1])
+        prefix = shorthand
+        if l != orig_l:
+            #print(l, orig_l, name, pname)
+            orig_shorthand = shorthands.get(orig_l, orig_l[:1])
+            prefix = "[{}/{}]".format(shorthand, orig_shorthand)
+            mc.append(["{}:{}".format(prefix, pname), lambda x=name, y=l: change_loglevel(x, y)])
+    # second pass, append the levels that are set to default
+    for name, pname, l, orig_l in sorted_names:
+        shorthand = shorthands.get(l, l[:1])
+        prefix = shorthand
+        if l == orig_l:
+            #print(l, orig_l, name, pname)
+            mc.append(["{}:{}".format(prefix, pname), lambda x=name, y=l: change_loglevel(x, y)])
+    return mc
 
 def config_logging():
     Menu([], i, o, contents_hook=get_menu_contents).activate()
