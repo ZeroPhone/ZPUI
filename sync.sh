f80c89c728220988574f6f7b96803cb6f8ef83bd
CRImier <crimier@yandex.ru>
2026-01-03 22:28:54 +0100

    sync: fixing sync scripts to sync configs from the systemwide copy beforehand; adding warnings for cases where sync.sh is ran from a possibly outdated local copy

--- sync.sh
+++ sync.sh
@@ -15,7 +15,16 @@ then
 fi
 
 mkdir -p "${INSTALL_DIR}"
+
+# syncing config files before sync, so that they don't get overwritten!
+rsync -av --include="*/" --include="*.yaml" --include "*.json" --exclude="*" "${INSTALL_DIR}"/ .
+
 rsync -av --delete ./ --exclude='*.pyc' "${INSTALL_DIR}"
 systemctl restart zpui.service
 #rm -rf ${TMP_DIR}
+
+echo "WARNING"
+echo "If you have updated the system-wide ZPUI copy using Settings=>Update ZPUI,"
+echo "it will no longer have those changes. No worries, though,"
+echo "run `git pull` now and then run `./sync.sh` again."
 exit 0
