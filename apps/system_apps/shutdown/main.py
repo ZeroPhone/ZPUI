46b87bf9c70d7c3faffd00d0bd16118415b09e39
CRImier <crimier@yandex.ru>
2026-06-14 08:09:30 +0200

    apps/system_apps/shutdown: pretty spinner on shutdown/reboot for better UX

--- apps/system_apps/shutdown/main.py
+++ apps/system_apps/shutdown/main.py
@@ -1,25 +1,38 @@
 menu_name = "Shutdown&reboot"
 
-from zpui_lib.ui import Menu, DialogBox
-from zpui_lib.actions import FirstBootAction
-
+from time import sleep
 from subprocess import call
 
+from zpui_lib.ui import Menu, DialogBox, LoadingIndicator
+from zpui_lib.actions import FirstBootAction
+from zpui_lib.helpers import ExitHelper
+
+# Auto-set by ZPUI
+i = None
+o = None
+
 context = None
+
 def set_context(c):
     global context
     context = c
     context.register_firstboot_action(FirstBootAction("reboot_after_firstboot", reboot_after_firstboot, depends=["change_wifi_country"], not_on_emulator=True))
 
 def shutdown():
-    o.clear()
-    o.display_data("Shutting down")
-    call(['shutdown', '-h', 'now'])
+    li = LoadingIndicator(i, o, message="Shutting down")
+    li.run_in_background()
+    #call(['shutdown', '-h', 'now'])
+    eh = ExitHelper(i, cb=li.stop).start()
+    while eh.do_run():
+        sleep(1)
 
 def reboot():
-    o.clear()
-    o.display_data("Rebooting")
-    call(['reboot'])
+    li = LoadingIndicator(i, o, message="Rebooting")
+    li.run_in_background()
+    #call(['reboot'])
+    eh = ExitHelper(i, cb=li.stop).start()
+    while eh.do_run():
+        sleep(1)
 
 def reboot_after_firstboot():
     choice = DialogBox("ync", i, o, message="Reboot to apply changes?", name="Reboot app firstboot reboot dialog").activate()
@@ -30,24 +43,12 @@ def reboot_after_firstboot():
     call(['reboot'])
     return True
 
+def callback():
+    contents = [
+      ["Shutdown", shutdown],
+      ["Reboot", reboot],
+      ["Exit", 'exit']
+    ]
 
-
-#Some globals for LCS
-main_menu = None
-callback = None
-#Some globals for us
-i = None
-o = None
-
-main_menu_contents = [
-["Shutdown", shutdown],
-["Reboot", reboot],
-["Exit", 'exit']
-]
-
-def init_app(input, output):
-    global main_menu, callback, i, o
-    i = input; o = output
-    main_menu = Menu(main_menu_contents, i, o, "Shutdown menu")
-    callback = main_menu.activate
+    Menu(contents, i, o, "Shutdown menu").activate()
 
